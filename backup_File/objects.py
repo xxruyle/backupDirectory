a7bd4ed2ce8da0519b4a13835f77c0d4424ec9a0
Theodicy1 <xrcnerd@gmail.com>
2022-07-17 13:42:46 -0500

    fixed copying .git folders

--- backup_File/objects.py
+++ backup_File/objects.py
@@ -26,13 +26,19 @@ class backupDirectory():
                 raise Exception("{Source -> Destination} syntax is not correct. Please refer to Example.txt")
             self.directorySyntax[paths[0].strip()] = paths[1].strip()
 
+    def copyFunction(self, src, dst):
+        if ".git" in src: 
+            pass
+        else:
+            copy2(src, dst)
+
 
     def copyDirectory(self): 
         self.splitDestination()
         for dirFolder in tqdm(list(self.directorySyntax), leave=True, desc=f"Backing Up: {self.directory_txt}"):
             new_folder = os.path.split(dirFolder)[-1]
             if os.path.isdir(dirFolder) and os.path.exists(dirFolder): 
-                shutil.copytree(dirFolder, fr"{self.directorySyntax[dirFolder]}/{new_folder}", dirs_exist_ok=True)
+                shutil.copytree(dirFolder, fr"{self.directorySyntax[dirFolder]}/{new_folder}", copy_function = self.copyFunction, dirs_exist_ok=True)
             
                 
     def listDirectoryTxt(self):  
